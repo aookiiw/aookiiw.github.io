82496a7a9352884264dd5cb703f324f7c97c591f
Karim <karim.sagandykov.2@gmail.com>
2024-02-14 11:52:34 +0100

    New commit

---
+++ valentine-day-main/script.js
@@ -0,0 +1,44 @@
+const alternatives = [
+  {text:"", images:"images/cat-01.gif"},
+  {text:"I promise you that it would be nice", images:"images/cat-02.gif"},
+  {text:"Think about it again", images:"images/cat-03.gif"},
+  {text:"Lets go, just say yes", images:"images/cat-04.gif"},
+  {text:"Don't let your fears say no", images:"images/cat-05.gif"},
+]
+const ohyes = {text:"I knew it <33", images:"images/cat-yes.gif"}
+const cat = document.querySelector('.cat')
+const text = document.querySelector('.text')
+const buttons = document.querySelectorAll('.button')
+const errorButton = document.querySelector('.button__error')
+
+let count = 0;
+
+function updateDisplay(item){
+  cat.src = item.images
+  text.innerHTML = item.text
+}
+
+errorButton.addEventListener('click', ()=>{
+  count = 0;
+  updateDisplay(alternatives[count])
+  buttons.forEach(btn => btn.style.display = 'inline-block')
+  errorButton.style.display = 'none'
+})
+
+buttons.forEach(button => {
+  button.addEventListener('click', ()=>{
+      if(button.textContent == "Yes"){
+          updateDisplay(ohyes)
+          buttons.forEach(btn => btn.style.display = 'none')
+      }
+      if(button.textContent == 'No'){
+          count++
+          if(count < alternatives.length){
+              updateDisplay(alternatives[count])
+          }else{
+              buttons.forEach(btn => btn.style.display = 'none')
+              errorButton.style.display = 'inline-block'
+          }
+      }
+  })
+})
